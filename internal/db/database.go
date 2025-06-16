b850ee7cde2273292019ef7a68772343887fd63a
romainstead <lolkek12042004@gmail.com>
2025-06-17 01:05:14 +0700

    new GET endpoints

--- internal/db/database.go
+++ internal/db/database.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"errors"
 	"fmt"
 	"gorm.io/gorm"
 	"mis_kursach_backend/internal/models"
@@ -8,29 +9,68 @@ import (
 
 func GetAllBookings(db *gorm.DB) ([]models.Booking, error) {
 	var bookings []models.Booking
-	err := db.Preload("Discount").Find(&bookings).Error
+	err := db.Preload("Discount").Preload("Status").Find(&bookings).Error
 	if err != nil {
-		return nil, fmt.Errorf("Error getting all bookings: %v", err)
+		return nil, fmt.Errorf("error getting all bookings: %v", err)
 	}
 	return bookings, nil
+}
+
+func GetBookingByID(db *gorm.DB, id int) (models.Booking, error) {
+	var booking models.Booking
+	result := db.Preload("Discount").Preload("Status").First(&booking, id)
+	if result.Error != nil {
+		if errors.Is(gorm.ErrRecordNotFound, result.Error) {
+			return models.Booking{}, fmt.Errorf("booking with id %v not found", id)
+		} else {
+			return models.Booking{}, fmt.Errorf("error getting booking: %v", result.Error)
+		}
+	}
+	return booking, nil
 }
 
 func GetAllComplaints(db *gorm.DB) ([]models.Complaint, error) {
 	var complaints []models.Complaint
 	err := db.Preload("Booking").Preload("Status").Find(&complaints).Error
 	if err != nil {
-		return nil, fmt.Errorf("Error getting all complaints: %v", err)
+		return nil, fmt.Errorf("error getting all complaints: %v", err)
 	}
 	return complaints, nil
+}
+
+func GetComplaintByID(db *gorm.DB, id int) (models.Complaint, error) {
+	var complaint models.Complaint
+	result := db.Preload("Booking").Preload("Status").First(&complaint, id)
+	if result.Error != nil {
+		if errors.Is(gorm.ErrRecordNotFound, result.Error) {
+			return models.Complaint{}, fmt.Errorf("complaint with id %v not found", id)
+		} else {
+			return models.Complaint{}, fmt.Errorf("error getting complaint: %v", result.Error)
+		}
+	}
+	return complaint, nil
 }
 
 func GetAllPayments(db *gorm.DB) ([]models.Payment, error) {
 	var payments []models.Payment
 	err := db.Preload("Booking").Preload("Method").Preload("Status").Find(&payments).Error
 	if err != nil {
-		return nil, fmt.Errorf("Error getting all complaints: %v", err)
+		return nil, fmt.Errorf("error getting all complaints: %v", err)
 	}
 	return payments, nil
+}
+
+func GetPaymentByID(db *gorm.DB, id int) (models.Payment, error) {
+	var payment models.Payment
+	result := db.Preload("Booking").Preload("Method").Preload("Status").First(&payment, id)
+	if result.Error != nil {
+		if errors.Is(gorm.ErrRecordNotFound, result.Error) {
+			return models.Payment{}, fmt.Errorf("payment with id %v not found", id)
+		} else {
+			return models.Payment{}, fmt.Errorf("error getting payment: %v", result.Error)
+		}
+	}
+	return payment, nil
 }
 
 type GetAllRoomsResult struct {
@@ -38,28 +78,14 @@ type GetAllRoomsResult struct {
 	CategoryName string `gorm:"column:category_name" json:"category_name"`
 	StateName    string `gorm:"column:state_name" json:"state_name"`
 	Capacity     int    `gorm:"column:capacity" json:"capacity"`
-	Prices       string `gorm:"type:json;column:prices" json:"prices"`
 }
 
 func GetAllRooms(db *gorm.DB) ([]GetAllRoomsResult, error) {
 	var rooms []GetAllRoomsResult
 	db.Raw(`
-        SELECT 
-            r.number,
-            c.name AS category_name,
-            s.name AS state_name,
-            c.capacity,
-            array_agg(
-                json_build_object(
-                    'day_code', t.day_code,
-                    'price', t.base_price
-                )
-            ) AS prices
-        FROM rooms r
-        JOIN room_categories c ON r.category_code = c.code
-        JOIN room_states s ON r.state_code = s.state_code
-        JOIN tariffs t ON t.category_code = c.code
-        GROUP BY r.number, c.name, s.name, c.capacity
+        SELECT r.number, rc.name AS category_name, rs.name AS state_name, rc.capacity FROM rooms r
+		JOIN room_categories rc ON rc.code = r.category_code
+		JOIN room_states rs ON rs.state_code = r.state_code
     `).Scan(&rooms)
 	return rooms, nil
 }
--- internal/db/pshandler.go
+++ internal/db/pshandler.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type PsHandler struct {
@@ -15,11 +16,14 @@ type PsHandler struct {
 func PsRoutes(db *gorm.DB) chi.Router {
 	r := chi.NewRouter()
 	handler := &PsHandler{db: db}
-
 	r.Get("/GetAllBookings", handler.GetAllBookings)
 	r.Get("/GetAllComplaints", handler.GetAllComplaints)
 	r.Get("/GetAllPayments", handler.GetAllPayments)
 	r.Get("/GetAllRooms", handler.GetAllRooms)
+
+	r.Get("/GetBookingByID/{id}", handler.GetBookingByID)
+	r.Get("/GetComplaintByID/{id}", handler.GetComplaintByID)
+	r.Get("/GetPaymentByID/{id}", handler.GetPaymentByID)
 	return r
 }
 
@@ -39,6 +43,23 @@ func (p *PsHandler) GetAllBookings(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (p *PsHandler) GetBookingByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("Error getting booking by id: %v", err)
+	}
+	booking, err := GetBookingByID(p.db, id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		log.Printf("Error getting booking: %v", err)
+	}
+	if err := json.NewEncoder(w).Encode(booking); err != nil {
+		http.Error(w, `{"error": "no bookings found"}`, http.StatusNotFound)
+	}
+}
+
 func (p *PsHandler) GetAllComplaints(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	complaints, err := GetAllComplaints(p.db)
@@ -54,6 +75,23 @@ func (p *PsHandler) GetAllComplaints(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (p *PsHandler) GetComplaintByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("Error getting complaint by id: %v", err)
+	}
+	complaint, err := GetComplaintByID(p.db, id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		log.Printf("Error getting complaint: %v", err)
+	}
+	if err := json.NewEncoder(w).Encode(complaint); err != nil {
+		http.Error(w, `{"error": "no complaints found"}`, http.StatusNotFound)
+	}
+}
+
 func (p *PsHandler) GetAllPayments(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	payments, err := GetAllPayments(p.db)
@@ -65,6 +103,23 @@ func (p *PsHandler) GetAllPayments(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 	}
 	if err := json.NewEncoder(w).Encode(payments); err != nil {
+		http.Error(w, `{"error": "no payments found"}`, http.StatusNotFound)
+	}
+}
+
+func (p *PsHandler) GetPaymentByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("Error getting payment by id: %v", err)
+	}
+	complaint, err := GetPaymentByID(p.db, id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		log.Printf("Error getting payment: %v", err)
+	}
+	if err := json.NewEncoder(w).Encode(complaint); err != nil {
 		http.Error(w, `{"error": "no payments found"}`, http.StatusNotFound)
 	}
 }
